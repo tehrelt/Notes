34107c5fed5483a12f3cae0272b2f59894de7ca3
relt <haxrelt@gmail.com>
2022-06-07 00:59:04 +0300

    Добавьте файлы проекта.

---
+++ Notes/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Notes.ViewModels.Base;
+
+internal class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string prop = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string prop = null)
+    {
+        if (Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(prop);
+        return true;
+    }
+}
---
+++ Notes/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Notes.ViewModels
+{
+    internal class MainWindowViewModel : Base.BaseViewModel
+    {
+        private string _title = "Notes";
+        public string Title
+        {
+            get { return _title; }
+            set { Set(ref _title, value); }
+        }
+    }
+}
